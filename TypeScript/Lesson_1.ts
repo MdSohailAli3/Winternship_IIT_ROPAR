c2ab7a60b9546a13978020df006812254c76510e
Mohammad Sohail Ali <145998001+MdSohailAli3@users.noreply.github.com>
2026-01-31 22:21:45 +0530

    first typescript case-study

---
+++ TypeScript/Lesson_1.ts
@@ -0,0 +1,17 @@
+//1
+let message: string = "Mohammad Sohail Ali";
+console.log("Hello " +message+", lets begin today's work...");
+//***************************************************
+
+
+//2
+let age : number = 21;
+console.log("You are "+age+" years old.");
+//***************************************************
+
+
+//3
+//let num: number = "hello";
+// it will give type error, because Type 'string' is not assignable to type 'number'.
+
+
